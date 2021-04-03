fdb523aa163e7a4c98613e55f0d1e3e19c625fe9
gennady.belogortsev <greeny7greeny@gmail.com>
2021-04-03 15:22:30 +0300

    finish dead-bones-removed mechanics

---
+++ src/char/states/CharStateBones.ts
@@ -0,0 +1,23 @@
+import {CharState} from "./CharState";
+import {CharAnimation, CharStateKey} from "../char-constants";
+import {CharAction} from "../../interface/char-actions-menu";
+
+export class CharStateSurrender extends CharState {
+    key = CharStateKey.SURRENDER
+
+    onStart(): Promise<any> {
+        this.char.isFleeing = true;
+        this.char.setAnimation(CharAnimation.SURRENDER);
+        this.char.actionsMenu.changeActiveButtons([
+            CharAction.RELEASE,
+            CharAction.ROB,
+            CharAction.TAKE_ALL,
+            CharAction.IMPRISON,
+            CharAction.KILL,
+            CharAction.DEVOUR,
+            CharAction.MAKE_FOOD,
+        ])
+        this.char.actionsMenu.show();
+        return Promise.resolve();
+    }
+}
